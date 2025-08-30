7c15e797b4b42e71866bcb949dd1b45462d4bb6b
lumenzaweb <marketing_lumenza@outlook.com>
2025-08-31 02:14:40 +0530

    fix: moible home hiding

--- src/HomeSection.jsx
+++ src/HomeSection.jsx
@@ -27,7 +27,7 @@ const HomeSection = () => {
     <section
       id="home"
       className="relative flex flex-col justify-center items-center text-center bg-cover bg-center overflow-hidden
-      h-[40vh] md:h-[80vh] pt-18" // ✅ smaller on mobile, navbar-safe padding
+      h-[40vh] md:h-[80vh] pt-14" // ✅ smaller on mobile, navbar-safe padding
     >
       {/* Background Slider */}
       <div className="absolute inset-0 z-0">
