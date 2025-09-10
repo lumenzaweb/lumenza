4ef93023376f6e0b1a0473c3ceb10562745acf46
lumenzaweb <marketing_lumenza@outlook.com>
2025-09-10 21:50:29 +0530

    Fix: product page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ProductsSection from "../ProductsSection";
 
-const ProductsPage = () => (
-  <div className="font-sans text-gray-800 pt-20">
-    <h2 className="text-3xl font-bold text-center mb-8">Our Products</h2>
-    <ProductsSection />
-  </div>
-);
+const ProductsPage = () => {
+  return (
+    <div className="pt-20">
+      <ProductsSection />
+    </div>
+  );
+};
 
 export default ProductsPage;
